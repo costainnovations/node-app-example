525039489798b1f61e83ec88b01a4b3903134bf9
giulianocosta <giuliano@costainnovations.com>
2026-03-26 15:40:50 -0300

    Test pipeline.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.get("/health", async (request, reply) => {
     return {
       status: "ok",
       timestamp: new Date().toISOString(),
-      random: Math.random(),
+      random: Math.random() + '_call',
       database: {
         connected: true,
         time: result.rows[0].time,
